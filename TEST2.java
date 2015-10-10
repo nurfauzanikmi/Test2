0c0c99fbe637d84ad932c69a3bebfff7df05cc09
nurfauzanikmi <nurfauzanikmi91@live.com>
2015-10-10 12:40:31 +0800

    Get IP Address

--- TEST2.java
+++ TEST2.java
@@ -5,10 +5,54 @@
  */
 package test2;
 
-/**
- *
+/*
  * @author Fauzan
+ * Purpose : Get an information from  NIC such as MAC address
  */
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 public class TEST2 {
-    
+    public static void main(String[] args){
+		
+	InetAddress ip;
+	try 
+        {	
+            ip = InetAddress.getLocalHost();
+            System.out.println(" IP address : " + ip.getAddress());
+            System.out.println(" Host address : " + ip.getHostAddress());
+            
+            getHostName()
+public String getCanonicalHostName()
+public byte[] getAddress()
+public String getHostAddress()
+
+            
+
+           // NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+
+          /*  byte[] mac = network.getHardwareAddress();
+
+            System.out.print("Current MAC address : ");
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < mac.length; i++) {
+                    sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));		
+            }
+            System.out.println(sb.toString());*/
+			
+	} catch (UnknownHostException e) {
+		
+		e.printStackTrace();
+		
+	} catch (SocketException e){
+			
+		e.printStackTrace();
+			
+	}
+	    
+   }
 }
