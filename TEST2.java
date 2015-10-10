9ccc980cf2b3925fd499ce52e8b8c959b635f768
nurfauzanikmi <nurfauzanikmi91@live.com>
2015-10-10 13:10:28 +0800

    Get MAC Address and convert to hexadecimal

--- TEST2.java
+++ TEST2.java
@@ -20,21 +20,34 @@ import java.util.logging.Logger;
 
 public class TEST2 {
     public static void main(String[] args){
-		
-	InetAddress ip;
 	try 
         {	
             //get NIC information
-            ip = InetAddress.getLocalHost();
+            InetAddress ip = InetAddress.getLocalHost();
             System.out.println("IP address : " + Arrays.toString(ip.getAddress()));
             System.out.println("Host address : " + ip.getHostAddress());
             System.out.println("Host Name : " + ip.getHostName());
+            System.out.println("Canonical Host Name : "+ip.getCanonicalHostName());
+            
+            /*byte[] iptest = new byte[] { 127, 0, 0, 1};
+            InetAddress addr = InetAddress.getByAddress(iptest);
+            String HostNameCanonical = addr.getCanonicalHostName();
+            System.out.println("Canonical Host Name (127.0.0.1) : "+HostNameCanonical);
+            */        
             
             //get MAC Address
             NetworkInterface nic = NetworkInterface.getByInetAddress(ip);
             byte[] MACaddr = nic.getHardwareAddress();
             //print out mac address
             System.out.print("Current MAC address : ");
+            //convert byte to hexadecimal
+            /*
+                % starts a format spec
+                0 means left-pad with zero
+                2 means 2 digits wide
+                X means hexadecimal output
+                s means string
+            */
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < MACaddr.length; i++) {
                 if(i < MACaddr.length - 1)
@@ -43,8 +56,6 @@ public class TEST2 {
                      sb.append(String.format("%02X%s", MACaddr[i], "-"));
             }
             System.out.println(sb.toString());
-            
-            
             
         }catch (UnknownHostException e) {
 		e.printStackTrace();
